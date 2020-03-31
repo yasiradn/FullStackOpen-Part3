e1c8e8caab95d732b210b3248f3e1754ac7b5dca
yasir <aff3ad@gmail.com>
2020-03-31 11:38:06 +0300

    3.1: Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,19 @@
+const experess = require('express')
+const app  = experess()
+
+let persons = [
+    { name: 'Arto Hellas', number: '040-123456' , id:1},
+    { name: 'Ada Lovelace', number: '39-44-5323523',id:2 },
+    { name: 'Dan Abramov', number: '12-43-234345',id:3},
+    { name: 'Mary Poppendieck', number: '39-23-6423122',id:4}
+]
+
+app.get('/api/persons', (req, res)=> {
+    res.json(persons)
+})
+
+const PORT = 3005
+
+app.listen(PORT, ()=>{
+    console.log(`Server on =>${PORT}`)
+})
